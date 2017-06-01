7f50a596e3ed3c515d9628a408277ea7399ae30c
davidrfreeman <davidfreeman@holos.solutions>
2017-05-31 21:43:33 -0600

    Added yarn lockfile and reset code to starter code from FCC

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+// server.js
+// where your node app starts
 
-let app = express();
+// init project
+var express = require('express');
+var app = express();
 
-let PORT = process.env.PORT || 3000;
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-app.use(cors({optionSuccessStatus: 200}));
-
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/timestamp/:date_string?', (req, res) => {
-  const dateVal = req.params.date_string;
 
-  let date,
-      UTCDate,
-      UNIXDate;
-  if(Number(dateVal)) {
-    UNIXDate = Number(dateVal);
-    date = new Date(dateVal);
-    UTCDate = date.toUTCString();
-
-  } else {
-    UNIXDate = Number(new Date());
-    UTCDate =new Date().toUTCString();
-  }
-
-  res.json({unix: UNIXDate, utc: UTCDate});
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
-app.listen(PORT, () => {
-  console.log('Your app is running on port', PORT);
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
