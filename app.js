6e63a3ac9aa7554f3f081a7f347ea6240a817ecc
davidrfreeman <davidfreeman@holos.solutions>
2017-06-01 22:36:50 -0600

    added logic to check for number string or nothing and return unix date and utc string

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+let PORT = process.env.PORT || 3000;
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -24,9 +25,35 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// timestamp api endpoint
+app.get("/api/timestamp/:date?", function (req, res) {
+  if(Number(req.params.date)) {
+
+    let date = new Date(parseInt(req.params.date));
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  } else if(req.params.date) {
+
+    date = new Date(req.params.date);
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  } else {
+    date = new Date();
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  }
+
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(PORT, function () {
+  console.log('Your app is listening on port ', PORT);
 });
